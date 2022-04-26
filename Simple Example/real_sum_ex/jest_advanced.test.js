3a80ffed47021bf78c592e6eb6ebddab688d05db
norhan <nora.moh.2011@gmail.com>
2022-04-26 03:53:36 +0200

    feat : use jest watchers && enhance test names

--- Simple Example/real_sum_ex/jest_advanced.test.js
+++ Simple Example/real_sum_ex/jest_advanced.test.js
@@ -1,18 +1,59 @@
+/*
+* Describe to collapse tests
+* "it" === "test"
+* BeforeAll , AfterAll
+* Watchers =>
+* -- toContain
+* -- toHaveLength
+
+ */
+
 const CalculateTotal = require('./fun')
 
-function testCalculateTotal(nums,res){
+ function testCalculateTotal(nums,res) {
+     // testInputs(nums)
+      testResults(nums,res)
+}
 
-    test(`it can sum ${nums||'null'} to equal ${res}`,()=>{
+////////////////////
+// Test Inputs
+///////////////////
+function testInputs(nums){
+    // beforeAll(()=>{
+        describe(`check inputs`,()=>{
+
+            test(`not have null`,()=>{
+
+                expect(nums).not.toContain(null)
+            })
+            test(`not have string`,()=>{
+                nums.some(n=>expect(n).not.toBe(true))
+            })
+        })
+    // }, 100);
+}
+
+////////////////////
+// Test Results
+///////////////////
+function testResults(nums,res){
+    test(`return ${res} if nums is ${nums}`,()=>{
         expect(CalculateTotal(nums)).toBe(res)
     })
 }
+
+
+//////////////////////
+// Tests Cases
+/////////////////////
 describe(`CalculateTotal results`,()=>{
-    describe.only('happy scenario',()=>{
+    describe('invalid inputs scenarios',()=>{
         testCalculateTotal(null,0)
         testCalculateTotal([30,null],30)
         testCalculateTotal([null,null],0)
+        testCalculateTotal([1,'2'],3)
     })
-    describe('nullable scenarios',()=>{
+    describe(' happy scenario',()=>{
         testCalculateTotal([10,2],12)
 
     })
